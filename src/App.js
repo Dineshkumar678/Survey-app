e15461137a2488e342e95d0299dd9e1e68d76541
Dinesh <dineshkumar1dec2@gmail.com>
2023-04-20 19:28:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Welcome from './Welcome';
+import Survey from './Survey';
+import Thankyou from './Thankyou';
+
+function App() {
+  return (
+  <div className="App">
+    <Switch>
+      <Route exact path="/">
+        <Welcome />
+      </Route>
+      <Route path="/survey">
+        <Survey/>
+      </Route>
+      <Route path="/thankyou">
+        <Thankyou/>
+      </Route>
+    </Switch>
+  </div>
+  );
+}
+
+export default App;
